7c638625fb2b216dea60748f7ab4c5a66407b121
Andrea <andiserm@gmail.com>
2022-12-29 19:17:02 -0300

    Agrego solucion al ejercicio plus

--- program.js
+++ program.js
@@ -87,3 +87,15 @@ function html(...args) {
 
 console.log(html`<b>${inputName} says</b>: "${inputMessage}"`)
 */
+
+// EJERCICIO PLUS DE
+/*
+const twoSum = (nums, target) => {
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] + nums[i + 1] == target) {
+      return [i, i + 1]
+    }
+  }
+  return []
+}
+*/
